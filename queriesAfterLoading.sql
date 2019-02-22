eb44b52e0ed40b018f77d81b54450ff23c0bc8e0
lucasnijder <45287148+lucasnijder@users.noreply.github.com>
2019-02-22 12:25:45 +0100

    Update queriesAfterLoading.sql

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,1 +1,3 @@
+***Sander's query***
 SELECT CourseOffers.*, Students.*, Degrees.* FROM StudentRegistrationsToDegrees, Students, Degrees, CourseOffers, StudentAssistants WHERE StudentRegistrationsToDegrees.StudentRegistrationId = 140 AND StudentRegistrationsToDegrees.StudentId = Students.StudentId AND StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId AND StudentAssistants.StudentRegistrationId = 140 AND CourseOffers.CourseOfferId = StudentAssistants.CourseOfferId;
+SELECT AVG(Grade) FROM CourseRegistrations WHERE StudentRegistrationId = 140
