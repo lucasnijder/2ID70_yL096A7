3a57e4bc2f93686a94984d8588a993819c251e0a
lucasnijder <45287148+lucasnijder@users.noreply.github.com>
2019-02-27 14:32:42 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,1 +1,6 @@
-WITH StudentsinDegree AS (SELECT distinct students.studentid, students.gender from students, studentregistrationstodegrees, degrees where students.studentid = studentRegistrationsToDegrees.studentid AND studentregistrationstodegrees.degreeid = degrees.degreeid AND Degrees.Dept = %1%) SELECT 100.0 * (SUM(CASE WHEN Gender='F' THEN 1 ELSE 0 END)::DECIMAL / COUNT(studentsindegree.studentid)) FROM studentsindegree;
+Q4 ==> WITH StudentsinDegree AS (SELECT distinct students.studentid, students.gender from students, studentregistrationstodegrees, degrees where students.studentid = studentRegistrationsToDegrees.studentid AND studentregistrationstodegrees.degreeid = degrees.degreeid AND Degrees.Dept = %1%) SELECT 100.0 * (SUM(CASE WHEN Gender='F' THEN 1 ELSE 0 END)::DECIMAL / COUNT(studentsindegree.studentid)) as percentage FROM studentsindegree;
+
+Q5 ==> 
+WITH nonnull AS (select * from courseregistrations where grade is not null) select courseid, SUM(CASE WHEN grade >= 5 THEN 1 ELSE 0 END)::DECIMAL / SUM(CASE WHEN grade < 5 THEN 1 ELSE 0 END)::DECIMAL FROM nonnull, courseoffers WHERE nonnull.courseofferid = courseoffers.courseofferid GROUP BY courseid ORDER BY courseid;
+
+Q6 ==> 
