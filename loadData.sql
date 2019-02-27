5a8831d52860927e4575d3a166b0314121dd1b33
lucasnijder <45287148+lucasnijder@users.noreply.github.com>
2019-02-27 10:47:38 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -1,7 +1,7 @@
 COPY Degrees(DegreeId, Dept, DegreeDescription, TotalESCTS) FROM '/mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;
 COPY Students(StudentId, StudentName, Address, BirthyearStudent, Gender) FROM '/mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;
 COPY StudentRegistrationsToDegrees(StudentRegistrationId,StudentId,DegreeId,RegistrationYear) FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
-COPY Teachers(TeacherId,TeacherName,Address,BirthyearTeacher,Gender) FROM ‘/mnt/ramdisk/tables/Teachers.table’ DELIMITER ‘,’ CSV HEADER;
+COPY Teachers(TeacherId,TeacherName,Address,BirthyearTeacher,Gender) FROM '/mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
 COPY Courses(CourseId, CourseName, CourseDescription, DegreeId, ECTS) FROM '/mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;
 COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM '/mnt/ramdisk/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
 COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
