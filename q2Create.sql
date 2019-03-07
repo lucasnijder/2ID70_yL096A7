0e79214b5bf7f71455eefcfc34438a1544c46e7d
lucasnijder <45287148+lucasnijder@users.noreply.github.com>
2019-03-07 14:05:17 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -18,3 +18,10 @@ CREATE MATERIALIZED VIEW GPA(StudentId, StudentRegistrationId, SumECTS, GPA) AS
 SELECT StudentId, StudentRegistrationId, sum(ECTS), (sum(ECTS * Grade))/(Sum(ECTS)) 
 FROM CourseOfferRegistrations, Courses, Degrees WHERE CourseOfferRegistrations.CourseId = Courses.courseId 
 AND Degrees.DegreeId = Courses.DegreeId AND Grade >=5 GROUP BY StudentId, StudentRegistrationId;
+
+CREATE MATERIALIZED VIEW MaterialGPA AS
+SELECT CourseOfferRegistrations.StudentRegistrationId, CAST(SUM(CourseOfferRegistrations.Grade * Courses.ECTS) AS FLOAT) / CAST(SUM(Courses.ECTS) AS FLOAT) as GPA
+FROM CourseOfferRegistrations, Courses
+WHERE CourseOfferRegistrations.CourseId = Courses.CourseId
+AND Grade >= 5
+GROUP BY CourseOfferRegistrations.StudentRegistrationId;
