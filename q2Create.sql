373b6fec3d9c0e77be8e0ebfd36820a4fc4f932b
lucasnijder <45287148+lucasnijder@users.noreply.github.com>
2019-03-08 16:24:07 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -2,7 +2,7 @@
 create index student_idx on courseOfferRegistrations(studentId);
 -- create a view for calculating the achieved ects per student per degree
 CREATE VIEW ects_per_degree AS
-SELECT StudentRegistrationsToDegrees.StudentRegistrationId, StudentRegistrationsToDegrees.DegreeId, SUM(ECTS) as currentects
+SELECT StudentRegistrationsToDegrees.StudentRegistrationId, StudentRegistrationsToDegrees.DegreeId, SUM(ECTS) as currentects, CAST(SUM(CourseOfferRegistrations.Grade * ECTS) AS FLOAT) / CAST(SUM(ECTS) AS FLOAT) as GPA
 FROM StudentRegistrationsToDegrees, CourseOfferRegistrations
 WHERE CourseOfferRegistrations.StudentRegistrationId = StudentRegistrationsToDegrees.StudentRegistrationId
 AND CourseOfferRegistrations.Grade >= 5
@@ -10,7 +10,7 @@ GROUP BY StudentRegistrationsToDegrees.StudentRegistrationId, StudentRegistratio
 
 -- create materialized view for completed degrees based on the ects per degree vs the total ects per degree
 CREATE MATERIALIZED VIEW CompletedDegrees AS
-SELECT ects_per_degree.StudentRegistrationId
+SELECT ects_per_degree.StudentRegistrationId, gpa
 FROM ects_per_degree, Degrees
 WHERE ects_per_degree.DegreeId = Degrees.DegreeId
 AND ects_per_degree.currentects >= Degrees.TotalECTS;
