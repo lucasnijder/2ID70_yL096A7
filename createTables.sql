f56c376b9c1fd5fdf1c765433141201d2e2292e9
lucasnijder <45287148+lucasnijder@users.noreply.github.com>
2019-03-08 14:23:00 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -3,7 +3,7 @@ CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address v
 CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear smallint);
 CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher smallint, Gender char(1));
 CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200), DegreeId int, ECTS smallint);
-CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year smallint, Quartile smallint);
+CREATE UNLOGGED TABLE CourseOffers_tmp(CourseOfferId int, CourseId int, Year smallint, Quartile smallint);
 CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
 CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int);
 CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade smallint);
